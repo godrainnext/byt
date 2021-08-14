59faf8cd25e6687848f7b6f542bca5a24886a487
JLdiesel <1317662314@qq.com>
2021-08-14 21:38:53 +0800

    123

--- src/screens/index.js
+++ src/screens/index.js
@@ -10,8 +10,8 @@ export { default as Optuijian } from './first/opera/optuijian'; //唱段推荐
 export { default as Opperson } from './first/opera/opperson'; //名角的唱段页面
 export { default as Opgroup } from './first/opera/opgroup'; //流派的唱段页面
 export { default as Actress } from './first/actress/index'; //名角个人页
-export { default as Album } from './first/album/idnex'//专辑页面
-export { default as MusicPlayer } from './first/musicplayer/musicPlayer'//歌曲播放页面
+export { default as Album } from './first/album'; //专辑页面
+export { default as MusicPlayer } from './first/musicplayer/musicPlayer'; //歌曲播放页面
 
 export { default as My } from './my/home/index'; //个人中心首页
 export { default as Login } from './my/Login'; //登录页面
