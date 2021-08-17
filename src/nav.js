ab4a75bdfc655d5200e639d0c6157eaee390b6f3
Jauua <2497679145@qq.com>
2021-08-17 19:34:22 +0800

    修改剧本推荐 商城的样式

--- src/nav.js
+++ src/nav.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 function Nav() {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode="none" initialRouteName="NewTimeLine">
+      <Stack.Navigator headerMode="none" initialRouteName="Tabbar">
 
         <Stack.Screen name="NewTimeLine" component={Screens.NewTimeLine} />
 
