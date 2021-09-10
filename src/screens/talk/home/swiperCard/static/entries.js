4ed75309c4a150a5c6755a4153de57db1b420559
Jauua <2497679145@qq.com>
2021-09-10 21:16:30 +0800

    11

--- src/screens/talk/home/swiperCard/static/entries.js
+++ src/screens/talk/home/swiperCard/static/entries.js
@@ -2,31 +2,16 @@ export const ENTRIES1 = [
     {
         title: 'Beautiful and dramatic Antelope Canyon',
         subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-        illustration: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.sc.enterdesk.com%2Fedpic%2Fda%2F20%2F51%2Fda2051af1a0a1d04836928d9e446b6fa.jpg&refer=http%3A%2F%2Fup.sc.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633154580&t=25a7bf2037c27e576df9e16999dbbe95'
+        illustration: require('../../../../../res/0901/论坛轮播1.jpg')
     },
     {
         title: 'Earlier this morning, NYC',
         subtitle: 'Lorem ipsum dolor sit amet',
-        illustration: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.616pic.com%2Fbg_w1180%2F00%2F26%2F37%2Fh445HJy1zQ.jpg&refer=http%3A%2F%2Fpic.616pic.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633154580&t=8805b67aea81f889b60ae5c2e5832331'
+        illustration: require('../../../../../res/0901/论坛轮播2.jpg')
     },
     {
         title: 'White Pocket Sunset',
         subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
-        illustration: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpicnew12.photophoto.cn%2F20180719%2Fchouxiangmeiguiwenli3ddaqixiaoqingxinhaibao-32858681_1.jpg&refer=http%3A%2F%2Fpicnew12.photophoto.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633154580&t=9d40d7730d8c873ffdf3e02df9418b97'
+        illustration: require('../../../../../res/0901/论坛轮播3.jpg')
     },
-    {
-        title: 'Acrocorinth, Greece',
-        subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-        illustration: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbpic.588ku.com%2Fback_pic%2F05%2F80%2F64%2F665c25b25461f61.jpg&refer=http%3A%2F%2Fbpic.588ku.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633154580&t=a2fcea33d6c554dfce578ad6b26003f2'
-    },
-    {
-        title: 'The lone tree, majestic landscape of New Zealand',
-        subtitle: 'Lorem ipsum dolor sit amet',
-        illustration: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbpic.588ku.com%2Fback_pic%2F19%2F03%2F24%2F144c0aa97126c7a7b8c1bb7f1333137a.jpg&refer=http%3A%2F%2Fbpic.588ku.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633154580&t=4cf28e44978fe502c8ea47a605d26b29'
-    },
-    {
-        title: 'Middle Earth, Germany',
-        subtitle: 'Lorem ipsum dolor sit amet',
-        illustration: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.51yuansu.com%2Fbackgd%2Fcover%2F00%2F04%2F85%2F5b618599f1e75.jpg%21%2Ffw%2F780%2Fquality%2F90%2Funsharp%2Ftrue%2Fcompress%2Ftrue&refer=http%3A%2F%2Fpic.51yuansu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633154580&t=9423bd31af6c36c861f09525b24e2529'
-    }
 ];
